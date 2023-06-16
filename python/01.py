6f2b47fd5b41e07b914bde9d64c885b8d04add49
thitipun7024 <130922221+thitipun7024@users.noreply.github.com>
2023-06-16 15:45:34 +0700

    first commit

---
+++ python/01.py
@@ -0,0 +1,10 @@
+""" user_list = ["thitipun", "beem", "pa"]
+for test in user_list:
+    print("hello",test) """
+
+""" for index in range(10):
+    print(user_list) """
+
+user_input = int(input())
+for i in range(user_input):
+    print("*"*(i+1))
